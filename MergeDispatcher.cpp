00d25162e788f3ebae37a03f186c9aefa0a46f6b
zenzhang <zenzhang@pptv.com>
2012-09-27 02:03:46 +0000

    1.2.0/src/ppbox/merge/MergeDispatcher.cpp 加入日志打印
    git-svn-id: http://svn01/repositories/r01/ppbox/branches/1.2.0@5044 b3a1389e-2159-5c43-9038-463bf877d0d9

--- MergeDispatcher.cpp
+++ MergeDispatcher.cpp
@@ -4,11 +4,14 @@
 #include "ppbox/merge/MergeDispatcher.h"
 #include "ppbox/merge/BigheadMp4Merge.h"
 
+#include <framework/logger/Logger.h>
 #include <framework/logger/StreamRecord.h>
 using namespace framework::logger;
 
 #include <boost/bind.hpp>
 using namespace boost::system;
+
+FRAMEWORK_LOGGER_DECLARE_MODULE_LEVEL("MergeDispatcher", 0);
 
 namespace ppbox
 {
@@ -34,61 +37,72 @@ namespace ppbox
 
         void MergeDispatcher::async_open_playlink(std::string const &playlink,ppbox::common::session_callback_respone const &resp)
         {
+            LOG_INFO("[async_open_playlink] playlink:"<<playlink);
             merge_->async_open(framework::string::Url(playlink),NULL,resp);
         }
 
 
         void MergeDispatcher::cancel_open_playlink(boost::system::error_code& ec)
         {
+            LOG_INFO("[cancel_open_playlink]");
             merge_->cancel(ec);
         }
 
         void MergeDispatcher::close_playlink(boost::system::error_code& ec)
         {
+            LOG_INFO("[close_playlink]");
             merge_->close(ec);
         }
 
         void MergeDispatcher::pause_moive(boost::system::error_code& ec)
         {
+            LOG_INFO("[pause_moive]");
             ec.clear();
         }
 
         void MergeDispatcher::resume_moive(boost::system::error_code& ec)
         {
+            LOG_INFO("[resume_moive]");
             ec.clear();
         }
 
         void MergeDispatcher::async_play_playlink(ppbox::common::Session* session,ppbox::common::session_callback_respone const &resp)
         {
+            LOG_INFO("[async_play_playlink]");
             player_->set(merge_,resp,session);
             post(boost::bind(&MergePlayer::doing,player_));
         }
 
         void MergeDispatcher::cancel_play_playlink(boost::system::error_code& ec)
         {
+            LOG_INFO("[cancel_play_playlink]");
             player_->stop();
         }
 
         void MergeDispatcher::async_buffering(ppbox::common::Session* session,ppbox::common::session_callback_respone const &resp)
         {
+            LOG_INFO("[async_buffering]");
             player_->set(merge_,resp);
             post(boost::bind(&MergePlayer::doing,player_));
         }
 
         void MergeDispatcher::cancel_buffering(boost::system::error_code& ec)
         {
+            LOG_INFO("[cancel_buffering]");
             player_->stop();
         }
 
         boost::system::error_code MergeDispatcher::get_media_info(
             ppbox::common::MediaInfo & info)
         {
+            LOG_INFO("[get_media_info]");
             return boost::system::error_code();
         }
 
         boost::system::error_code MergeDispatcher::get_play_info(
             ppbox::common::PlayInfo & info)
         {
+            LOG_INFO("[get_play_info]");
             return boost::system::error_code();
         }
 
